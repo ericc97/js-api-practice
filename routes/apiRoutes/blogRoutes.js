56e51c54034ac71476014830d66436b5badcc254
Eric Coleman <ericcoleman9217@gmail.com>
2022-05-12 18:26:53 -0500

    added 2 get requests

--- routes/apiRoutes/blogRoutes.js
+++ routes/apiRoutes/blogRoutes.js
@@ -4,23 +4,54 @@ const axios = require('axios');
 const { response } = require('express');
 
 
-router.get('/posts/:tag/:sortBy', (req, res) => {
+router.get('/posts/:tag', (req, res) => {
+	if(req.params.tag === ' '){
+		console.error('Tag must be included')
+	}
 	
-	axios.get(`https://api.hatchways.io/assessment/blog/posts?tag=${req.params.tag}&sort=${req.params.sortBy}`, 
-	// { 
-	// 	params: { 
-	// 		tag: req.params.tag,
-	// 		sort: req.params.ordering,
-	// 		// sortBy: req.params.sortBy,
-	// 		// direction: req.params.direction
-	// 	}
+	axios.get(`https://api.hatchways.io/assessment/blog/posts?`, 
+	{ 
 		
-	// }
+		params: { 
+			tag: req.params.tag,
+		}
+		
+	}
 	)
 		.then(response => {
 			let data = response.data
-			console.log(data)
-			res.send(data)
+			let posts = [];
+			posts.push(data)
+			
+			// function to sort posts based on sortBy and direction parameters
+			function sortPosts(data, sortBy, direction) {
+  			return data.sort((a, b) => {
+    			if (direction === 'asc') {
+      			return a[sortBy] - b[sortBy];
+    			} else {
+      			return b[sortBy] - a[sortBy];
+    			}
+  		})
+
+				function getPostsByTags({ query }, res) {
+					//destructure and set default params
+					const tags = query.tags;
+					const sortBy = query.sortBy ? query.sortBy : 'id';
+					const direction = query.direction ? query.direction : 'asc';
+
+					// check for tags
+					if(!tags){
+						res.status(400).json({ error: `Tag must be included`})
+					}
+
+					const sortedPosts = sortPosts(posts, sortBy, direction)
+
+					res.send(sortedPosts)
+				}
+				
+			
+		}
+		
 			
 		})
 		.catch(error => {
